870ce20984dfb50e80a8cbbbcc903d72e4f3c952
MatsumaKeketso <49682202+MatsumaKeketso@users.noreply.github.com>
2019-08-01 16:52:52 +0200

    Layout 90% done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'login', loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)},
+  { path: 'register', loadChildren: () => import('./pages/register/register.page').then( m => m.RegisterPage)}
 ];
 
 @NgModule({
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+// import { ModalController } from '@ionic/angular';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,7 +7,9 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
   classname = 'roominmage';
-  items = [1,2,3,4,5,6,7,8,9,0,121,3,525,23,52,52,6,547,568,6787,97,64,562,4,23,41,23,12,12,4,235,34,65,457,56,76,78,67,978,5,6,345,5,,23,5,23,4,2,35,67];
-  constructor() {} 
+  // tslint:disable-next-line: max-line-length
+  items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 121, 3, 525, 23, 52, 52, 6, 547, 568, 6787, 97, 64, 562, 4, 23, 41, 23, 12, 12, 4, 235, 34, 65, 457, 56, 76, 78, 67, 978, 5, 6, 345, 5, , 23, 5, 23, 4, 2, 35, 67];
+
+  constructor() {}
 
 }
