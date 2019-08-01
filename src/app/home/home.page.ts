0c961717cb10213f2bb8da3b5194c273ed3692f1
449sharon <50619633+449sharon@users.noreply.github.com>
2019-08-01 17:57:21 +0200

    logout is working
    Co-Authored-By: Keketso Matsuma <matsumakeketso@users.noreply.github.com>

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../services/user/auth.service';
+import { Router } from '@angular/router';
+
 // import { ModalController } from '@ionic/angular';
 @Component({
   selector: 'app-home',
@@ -10,6 +13,17 @@ export class HomePage {
   // tslint:disable-next-line: max-line-length
   items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 121, 3, 525, 23, 52, 52, 6, 547, 568, 6787, 97, 64, 562, 4, 23, 41, 23, 12, 12, 4, 235, 34, 65, 457, 56, 76, 78, 67, 978, 5, 6, 345, 5, , 23, 5, 23, 4, 2, 35, 67];
 
-  constructor() {}
+  constructor(
+    private authService:AuthService,
+    private router:Router
+
+  ) {}
+
+logOut(): void{
+  this.authService.logoutUser().then(() =>{
+    this.router.navigateByUrl('login');
+  });
 
 }
+}
+
