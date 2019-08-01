c479687650c0162caf2e030bcfb168c3ed307aa1
449sharon <50619633+449sharon@users.noreply.github.com>
2019-08-01 16:53:40 +0200

    initial commit

--- src/app/services/room/room.service.ts
+++ src/app/services/room/room.service.ts
@@ -1,21 +1,16 @@
 import { Injectable } from '@angular/core';
 import * as firebase from 'firebase';
-import  'firebase/auth';
-import 'firebase/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RoomService {
-  public roomListRef:firebase.firestore.CollectionReference;
-
+  db = firebase.firestore();
+  roomInfo;
   constructor() { 
     firebase.auth().onAuthStateChanged(user =>{
-      if (user{
-        this.roomListRef =firebase
-        .firestore()
-        .collection(`userProfile/${user.uid}/roomList`);
-
+      if (user) {
+        this.db.collection('users').doc(user.uid).set(this.roomInfo);
       }
     })
   }
