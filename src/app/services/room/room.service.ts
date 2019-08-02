09b7b19ad3c865c0d23d5a5ca640f803fb339892
449sharon <50619633+449sharon@users.noreply.github.com>
2019-08-02 10:32:18 +0200

    back end for adding room details

--- src/app/services/room/room.service.ts
+++ src/app/services/room/room.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import * as firebase from 'firebase';
+import 'firebase/auth';
+import { stringify } from 'querystring';
+
 
 @Injectable({
   providedIn: 'root'
@@ -8,11 +11,37 @@ export class RoomService {
   db = firebase.firestore();
   roomInfo;
   constructor() { 
-    firebase.auth().onAuthStateChanged(user =>{
-      if (user) {
-        this.db.collection('users').doc(user.uid).set(this.roomInfo);
+    // firebase.auth().onAuthStateChanged(user =>{
+    //   if (user) {
+    //     this.db.collection('users').doc(user.uid).set(this.roomInfo);
+    //   }
+    // })
+  }
+  addRoom(
+    roomImage: string,
+    roomName:string,
+    roomPrice: number,
+    roomDescription: string,
+    roomFeatures : string,
+    ): Promise <firebase.firestore.DocumentReference>{
+      return this.roomInfo.add({
+        image: roomImage,
+        name: roomName,
+        price:roomPrice,
+        description:roomDescription,
+        features:roomFeatures
+      });
+    }
+    getRoomList():firebase.firestore.CollectionReference{
+      return this.roomInfo;
       }
-    })
+
+getRoomDetail(roomId:string): firebase.firestore.DocumentReference{
+  return this.roomInfo.doc(roomId);
+}
+
+
+
   }
   
-}
+
